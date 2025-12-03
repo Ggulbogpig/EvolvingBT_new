459520a8a8500e3582db0f75856a37b5b69bb0ee
현민 <143337620+Ggulbogpig@users.noreply.github.com>
2025-12-04 00:36:31 +0900

    Test_map 생성

---
+++ SurvivalGame/Source/SurvivalGame/Private/MyClass.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020 Tom Looman, and additional copyright holders as specified in LICENSE.md
+
+
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ SurvivalGame/Source/SurvivalGame/Public/MyClass.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Tom Looman, and additional copyright holders as specified in LICENSE.md
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SURVIVALGAME_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
